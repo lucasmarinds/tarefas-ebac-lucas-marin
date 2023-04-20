52051437c0b9328ff40dfce32714d243c2685e3c
Lucas <lucasmartinsmarin24@gmail.com>
2023-04-20 00:10:29 -0300

    Exercicio do modulo 9 mas finalizacao modulo 9

---
+++ exercicios-ebac/src/tarefa_modulo_9/ExercicioMod9.java
@@ -0,0 +1,10 @@
+package tarefa_modulo_9;
+
+public class ExercicioMod9 {
+    public static void main(String[] args) {
+        long valor = 2000000000000000000l;
+        System.out.println(valor);
+        Long valorWrapper = valor;
+        System.out.println(valorWrapper);
+    }
+}
