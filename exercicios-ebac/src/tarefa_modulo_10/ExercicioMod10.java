f37251c861e0c52fdeaa68a529ceecda8373b908
Lucas <lucasmartinsmarin24@gmail.com>
2023-04-22 01:43:17 -0300

    Adição de Exemplo de switch case e adicao de exercicio

---
+++ exercicios-ebac/src/tarefa_modulo_10/ExercicioMod10.java
@@ -0,0 +1,10 @@
+package tarefa_modulo_10;
+
+import java.util.Scanner;
+
+public class ExercicioMod10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
