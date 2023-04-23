4d06d318d3d256684961ddc746b4356d4c502b77
Lucas <lucasmartinsmarin24@gmail.com>
2023-04-22 23:21:41 -0300

    Adição de exercicio utilizando lacos e condicao

--- exercicios-ebac/src/tarefa_modulo_10/ExercicioMod10.java
+++ exercicios-ebac/src/tarefa_modulo_10/ExercicioMod10.java
@@ -5,6 +5,20 @@ import java.util.Scanner;
 public class ExercicioMod10 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Digite quantidade de notas para media");
+        int quantidadeDeNotas = scanner.nextInt();
+        double somaNotas = 0;
+        for (int i = 1; i <= quantidadeDeNotas; i++){
+            System.out.println("Valor da nota " + i);
+            somaNotas += scanner.nextInt();
+        }
+        double media = somaNotas / quantidadeDeNotas;
+        if (media >= 7) {
+            System.out.println("APROVADO");
+        } else if (media >= 5) {
+            System.out.println("RECUPERACAO");
+        } else {
+            System.out.println("REPROVADO");
+        }
     }
 }
